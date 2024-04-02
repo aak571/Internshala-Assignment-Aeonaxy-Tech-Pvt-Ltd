9b2aa19213301fc975e9f5fdb70f31242ba59d9d
Aakash <aakash.kavi10@gmail.com>
2024-04-02 21:33:01 +0530

    Basic Controllers

---
+++ backend/src/controllers/user.controllers.js
@@ -0,0 +1,15 @@
+const create_account = (req, res, next) => {
+
+}
+
+const get_user = (req, res, next) => {
+
+}
+
+const edit_user = (req, res, next) => {
+
+}
+
+const delete_user = (req, res, next) => {
+
+}
