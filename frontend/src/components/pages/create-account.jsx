dc21fd019132d0737883b70f2e76e30cbfa784de
Aakash <aakash.kavi10@gmail.com>
2024-04-06 08:14:46 +0530

    Made the cover image and register content to stack upon each other for small screen devices

--- frontend/src/components/pages/create-account.jsx
+++ frontend/src/components/pages/create-account.jsx
@@ -1,9 +1,9 @@
 const CreateAccount = () => {
     return (
         <div>
-            <div className="flex">
+            <div className="md:flex">
                 <div className="w-3/6 ">
-                    <img className="h-screen w-screen" src="https://images.unsplash.com/photo-1597551681492-10c86e481548?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                    <img className="h-screen w-screen" alt="Cover" src="https://images.unsplash.com/photo-1597551681492-10c86e481548?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
                 </div>
                 <div className="absolute w-32 ml-2 mt-1 top-0 left-0 md:w-60 lg:w-80">
                     <h1 className="italic text-2xl font-bold underline decoration-wavy text-green-300 text-center">Unsplash</h1>
