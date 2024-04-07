1f185067b4e81451431e28f84c0900d373553d84
Aakash <aakash.kavi10@gmail.com>
2024-04-07 15:50:46 +0530

    Added React Routes for 'create-account', 'create-profile' and 'email-verify' components

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,14 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import CreateAccount from "./components/pages/create-account.jsx"
 import CreateProfile from "./components/pages/create-profile.jsx"
 import EmailVerify from "./components/pages/email-verify.jsx"
 
 const App = () => {
     return (
-        <div>
-            {/* <CreateAccount /> */}
-            {/* <CreateProfile /> */}
-            <EmailVerify />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/'>
+                    <Route index element={<CreateAccount />} />
+                    <Route path='/create-profile' element={<CreateProfile />} />
+                    <Route path='/verify-email' element={<EmailVerify />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
